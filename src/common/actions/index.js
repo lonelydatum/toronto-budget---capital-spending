c494a4e72abf50368a06d6bcc66052499a9f2c0e
Gar <gar@lonelydatu.com>
2016-04-05 09:32:44 -0400

    server rendering issues

--- src/common/actions/index.js
+++ src/common/actions/index.js
@@ -1,49 +1,3 @@
-// export const SET_COUNTER = 'SET_COUNTER'
-// export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
-// export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
-
-// export function set(value) {
-//   return {
-//     type: SET_COUNTER,
-//     payload: value
-//   }
-// }
-
-// export function increment() {
-//   return {
-//     type: INCREMENT_COUNTER
-//   }
-// }
-
-// export function decrement() {
-//   return {
-//     type: DECREMENT_COUNTER
-//   }
-// }
-
-// export function incrementIfOdd() {
-//   return (dispatch, getState) => {
-//     const { counter } = getState()
-
-//     if (counter % 2 === 0) {
-//       return
-//     }
-
-//     dispatch(increment())
-//   }
-// }
-
-// export function incrementAsync(delay = 1000) {
-//   return dispatch => {
-//     setTimeout(() => {
-//       dispatch(increment())
-//     }, delay)
-//   }
-// }
-
-
-
-
 
 
 
@@ -54,7 +8,13 @@ import * as type from '../constants/ActionTypes'
 let data = []
 if (process.env.BROWSER) {
   data = budget_data  
+  console.log("browser");
+}else{
+  data = require('../data/budget_clean.json')
+  console.log("server");
 }
+
+
 
 
 
